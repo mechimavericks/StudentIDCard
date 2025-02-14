27bf35ba529def6962c0f80fe03104650dc3204c
unknown <yangsinglimbu@gmail.com>
2025-02-14 08:37:25 +0545

    added squid game music in backgroud

--- generate_team/src/Home.jsx
+++ generate_team/src/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const audioRef = useRef(
+    new Audio("/Squid Game Morning Alarm Clock Music.mp3")
+  );
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (audioRef.current) {
+      audioRef.current.loop = true;
+      audioRef.current
+        .play()
+        .catch((error) => console.log("Playback error:", error));
+    }
+
+    setTimeout(() => {
+      navigate("/team");
+    }, 500);
+  };
   return (
     <div className="h-full flex items-center flex-col justify-around">
       <div>
@@ -14,11 +31,13 @@ const Home = () => {
       <h2 className="text-6xl text-white font-bold text-center ">
         TRESSURE HUNT 2.0
       </h2>
-      <Link to="/team">
-        <button className=" bg-white text-black hover:bg-gray-200 py-4 px-10 rounded-full text-5xl  font-bold cursor-pointer">
-          CHOOSE TEAM
-        </button>
-      </Link>
+
+      <button
+        onClick={handleClick}
+        className=" bg-white text-black hover:bg-gray-200 py-4 px-10 rounded-full text-5xl  font-bold cursor-pointer"
+      >
+        CHOOSE TEAM
+      </button>
     </div>
   );
 };
